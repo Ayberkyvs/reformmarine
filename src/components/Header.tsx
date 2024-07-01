51ed976dba0bedad505c23bffa394618b4687c95
Ayberk Yavas <ayberkyavas9@gmail.com>
2024-07-01 17:58:42 +0300

    improvements

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,8 @@
-import { FaSquareWhatsapp, FaSquareInstagram, FaLinkedin, FaGooglePlus, FaPhone } from "react-icons/fa6";
-import { IoMdMail } from "react-icons/io";
-// import BlueLogo from "../assets/logo-blue.png"
-// import WhiteLogo from "../assets/logo-white.png"
+import { FaWhatsapp, FaInstagram, FaLinkedin, FaGooglePlus, FaPhone } from "react-icons/fa6";
+import { IoIosMail } from "react-icons/io";     
 import DesktopWhiteLogo from "../assets/logo-white-desktop.png"
 import Show from "./Show";
-// import Hide from "./Hide";
 import { Link } from "react-router-dom";
-// import { BiPhone } from "react-icons/bi";
 
 export default function Header() {
   return (
@@ -14,17 +10,17 @@ export default function Header() {
         <div className="flex w-full h-fit bg-orange-50 items-center justify-center">
             <div className="flex w-full max-w-screen-xl h-[25px] lg:h-[30px] justify-between items-center p-2">
                 <ul className="flex items-center gap-2 md:gap-5 w-full h-full justify-between lg:justify-start text-xs">
-                    <li className="flex text-[9px] md:text-xs text-orange-800"><a href="tel:+90 537 344 25 66" className="flex items-center gap-[5px]" target="_blank"><FaPhone className="w-[14px]" /> +90 537 344 25 66</a></li>
-                    <li className="flex text-[9px] md:text-xs text-orange-800"><a href="tel:+90 532 567 53 50" className="flex items-center gap-[5px]" target="_blank"><FaPhone className="w-[14px]" /> +90 532 567 53 50</a></li>
-                    <li className="flex text-[9px] md:text-xs text-orange-800"><a href="mailto:info@reformmarine.com" className="flex items-center gap-[5px]" target="_blank"><IoMdMail className="w-[14px]" /> info@reformmarine.com</a></li>
+                    <li className="flex text-[9px] md:text-xs text-orange-800"><a href="tel:+90 537 344 25 66" className="flex items-center gap-[5px]" target="_blank"><FaPhone className="w-[14px] h-[14px]" /> +90 537 344 25 66</a></li>
+                    <li className="flex text-[9px] md:text-xs text-orange-800"><a href="tel:+90 532 567 53 50" className="flex items-center gap-[5px]" target="_blank"><FaPhone className="w-[14px] h-[14px]" /> +90 532 567 53 50</a></li>
+                    <li className="flex text-[9px] md:text-xs text-orange-800"><a href="mailto:info@reformmarine.com" className="flex items-center gap-[5px]" target="_blank"><IoIosMail className="w-[20px] h-[20px]"/> info@reformmarine.com</a></li>
                 </ul>
                 <Show>
-                    <ul className="flex items-center gap-1 w-fit h-full text-md text-orange-800">
-                        <a href="https://wa.me/905373442566" target="_blank">
-                            <li><FaSquareWhatsapp/></li>
+                    <ul className="flex items-center gap-[5px] w-fit h-full text-base text-orange-800">
+                        <a href="https://wa.me/905325675350" target="_blank">
+                            <li><FaWhatsapp/></li>
                         </a>
                         <a href="https://www.instagram.com/reformmarine/" target="_blank">
-                            <li><FaSquareInstagram /></li>
+                            <li><FaInstagram /></li>
                         </a>
                         <a href="https://www.linkedin.com/company/reformmarine" target="_blank">
                             <li><FaLinkedin /></li>
